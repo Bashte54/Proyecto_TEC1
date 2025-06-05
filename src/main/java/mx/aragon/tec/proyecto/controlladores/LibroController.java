973c82a97e26a4b1cd3dea303677b268ea39e3a5
Bashte54 <sebastian.mendozarosas54@gmail.com>
2025-06-04 21:22:42 -0600

    metodo guardar y plantilla de formulario

--- src/main/java/mx/aragon/tec/proyecto/controlladores/LibroController.java
+++ src/main/java/mx/aragon/tec/proyecto/controlladores/LibroController.java
@@ -1,9 +1,14 @@
 package mx.aragon.tec.proyecto.controlladores;
 
+import mx.aragon.tec.proyecto.modelos.Libro;
 import mx.aragon.tec.proyecto.services.LibroService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,7 +21,22 @@ public class LibroController {
     @GetMapping("/")
     public String home() {return "home";}
 
-    @GetMapping("/nuevo/")
-    public String nuevo() {return "nuevo";}
+    @GetMapping("/nuevo")
+    public String nuevo(Model model) {
+        model.addAttribute("libro", new Libro());
+        return "formlibro";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(
+            //@RequestParam String titulo
+            @ModelAttribute Libro libro
+    ){
+        LoggerFactory.getLogger(getClass()).info("Guardando libro: " + libro);
+        libroService.guardarLibro(libro);
+        return "redirect:/libros/nuevo?exito";
+    }
+
+
 
 }
